dc5c2bd7f2894ad167ec708bd0507ff6d7d5a658
matheusmsgomes23 <matheusmsgomes@gmail.com>
2026-03-08 19:44:02 -0400

    Create R00_IDS03_J.java

---
+++ R00_IDS03_J.java
@@ -0,0 +1,33 @@
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+public class R00_IDS03_J {
+
+    static Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());
+
+    public static void main(String[] args) {
+
+        String username = "user<script>";
+        boolean loginSuccessful = false;
+
+        // Noncompliant code
+        if (loginSuccessful) {
+            logger.severe("User login succeeded for: " + username);
+        } else {
+            logger.severe("User login failed for: " + username);
+        }
+
+        // Compliant code
+        if (loginSuccessful) {
+            logger.severe("User login succeeded for: " + sanitizeUser(username));
+        } else {
+            logger.severe("User login failed for: " + sanitizeUser(username));
+        }
+    }
+
+    public static String sanitizeUser(String username) {
+        return Pattern.matches("[A-Za-z0-9]+", username)
+                ? username
+                : "unauthorized user";
+    }
+}
