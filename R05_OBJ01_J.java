e740e930dbb5a6f7607502f728731794dc4e061c
matheusmsgomes23 <matheusmsgomes@gmail.com>
2026-03-08 20:46:18 -0400

    Create R05_OBJ01_J.java

---
+++ R05_OBJ01_J.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class R05_OBJ01_J {
+
+    private Date date = new Date();
+
+    // Noncompliant
+    public Date getDateBad() {
+        return date;
+    }
+
+    // Compliant
+    public Date getDate() {
+        return new Date(date.getTime());
+    }
+
+    public static void main(String[] args) {
+        R05_OBJ01_J obj = new R05_OBJ01_J();
+        System.out.println(obj.getDate());
+    }
+}
